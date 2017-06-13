d705e63837a926a00fa45680f77804dd8bbfbf5d
alexand1r <s123189@gmail.com>
2017-06-14 00:42:02 +0300

    added function for falling down of blocks when a row is destroyed

--- Tetris/Tetris.cs
+++ Tetris/Tetris.cs
@@ -12,6 +12,8 @@ namespace Tetris
     {
         public static bool[,] curPiece;
         public static bool[,] matrix;
+        public static int MATRIX_ROWS = 22;
+        public static int MATRIX_COLS = 10;
         public static int curX;
         public static int curY;
 
@@ -21,7 +23,6 @@ namespace Tetris
         public static ConsoleKeyInfo key;
         public static bool isKeyPressed = false;
 
-
         public static int LineCleared = 0;
 
         public static void StartGame()
@@ -30,7 +31,7 @@ namespace Tetris
             gameOver = false;
 
             var blocks = Blocks.createBlocks();
-            matrix = new bool[22, 10];
+            matrix = new bool[MATRIX_ROWS, MATRIX_COLS];
 
             // filling matrix
             HelperFunctions.FillMatrix(matrix);
@@ -47,6 +48,7 @@ namespace Tetris
                 pieces.Push(HelperFunctions.PickRandomBlock(blocks, rnd));
                 HelperFunctions.NextBlock(pieces.Peek());
 
+                // setting new piece's coordinates
                 curPiece = newPiece;
                 curX = 0;
                 curY = matrix.GetLength(1) / 2 - 1;
@@ -88,26 +90,48 @@ namespace Tetris
             HelperFunctions.AskForRestart();
         }
 
-        private static void ClearLines()
+        public static void ClearLines()
         {
             for (int row = matrix.GetLength(0) - 1; row > 0; row--)
             {
                 int counter = 0;
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    if (matrix[row, col] == true)
+                    if (matrix[row, col])
                     {
                         counter++;
                     }
-                    if (counter == 10)
+                    // clear the row
+                    if (counter == MATRIX_COLS)
                     {
                         LineCleared++;
-                        for (int i = 0; i < 10; i++)
+                        for (int i = 0; i < MATRIX_COLS; i++)
                         {
                             matrix[row, i] = false;
                         }
                         Console.SetCursorPosition(47, 6);
                         Console.Write(LineCleared);
+
+                        // fall down
+                        FallDown(row);
+
+                        // check for another empty full row is created (recursion)
+                        ClearLines();
+                    }
+                }
+            }
+        }
+
+        public static void FallDown(int row)
+        {
+            for (int column = 0; column < matrix.GetLength(1); column++)
+            {
+                for (int curRow = row - 1; curRow >= 0; curRow--)
+                {
+                    if (matrix[curRow, column])
+                    {
+                        matrix[curRow + 1, column] = matrix[curRow, column];
+                        matrix[curRow, column] = false;
                     }
                 }
             }
